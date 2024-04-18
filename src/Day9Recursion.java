bef4c6f8ff16785324bbb84d7bbf7b2bfe1f2730
jaminel <jamineltechnology@gmail.com>
2024-04-18 16:25:28 +0100

    day9

--- src/Day9Recursion.java
+++ src/Day9Recursion.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Day9Recursion {
+
+
+public class Day9Recursion {
+
+    public int factorial(int n){
+        if(n==0){
+            return 1;
+        }
+        else {
+            return factorial(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
